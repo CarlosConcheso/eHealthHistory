6fd7be4b7cca2e4ade5fb5ceba6ac07cb4f79571
Carlos Concheso Cubillas <carlos.concheso.cubillas@gmail.com>
2022-03-08 19:26:52 +0100

    Dos entidades necesarias añadidas

---
+++ app/src/main/java/com/example/ehealthhistory/data/model/User/Rol.java
@@ -0,0 +1,28 @@
+package com.example.ehealthhistory.data.model.User;
+
+public class Rol {
+
+    private String username;
+    private String name;
+
+    public Rol(String username, String name) {
+        this.username = username;
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/example/ehealthhistory/data/model/User/User.java
@@ -0,0 +1,28 @@
+package com.example.ehealthhistory.data.model.User;
+
+public class User {
+
+    private String username;
+    private String pass;
+
+    public User(String username, String pass) {
+        this.username = username;
+        this.pass = pass;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
